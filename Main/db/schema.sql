9efc73e2534acca2ea37b13ed4085a30f3ad5d29
TristanM225 <TristanM225@gmail.com>
2023-06-27 16:09:53 -0700

    created schema tables

---
+++ Main/db/schema.sql
@@ -0,0 +1,32 @@
+-- need to create tables for DEPARTMENT, ROLES, EMPLOYEES, (ADD DEPARTMENT, ADD A ROLE, ADD EMPLOYEE, UPDATE EMPLOYEE ROLE)
+
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+-- Creating a table for departments
+CREATE TABLE deparment (
+  id INT NOT NULL AUTO_INCREMENT,
+  deparment_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Creating a table for employees
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creating a table for roles
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL NOT NULL,
+    deparment_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
